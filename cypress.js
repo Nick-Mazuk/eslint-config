ea532a949f90860690ac3c6dbb3a0ae74b226594
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2021-05-01 14:26:08 -0700

    Limit cypress config to only cypress files

--- cypress.js
+++ cypress.js
@@ -5,12 +5,17 @@ module.exports = {
         'plugin:cypress/recommended',
         'plugin:chai-friendly/recommended',
     ],
-    rules: {
-        'cypress/assertion-before-screenshot': 'error',
-        'cypress/no-force': 'error',
-        'import/unambiguous': 'off',
-        'promise/catch-or-return': 'off',
-        'promise/always-return': 'off',
-        '@typescript-eslint/no-floating-promises': 'off',
-    },
+    overrides: [
+        {
+            files: ['*.test.ts', 'cypress/**/*.ts'],
+            rules: {
+                'cypress/assertion-before-screenshot': 'error',
+                'cypress/no-force': 'error',
+                'import/unambiguous': 'off',
+                'promise/catch-or-return': 'off',
+                'promise/always-return': 'off',
+                '@typescript-eslint/no-floating-promises': 'off',
+            },
+        },
+    ],
 }
